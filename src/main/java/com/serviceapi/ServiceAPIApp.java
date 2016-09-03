2419132b46f20da7bb4fc1179a9cd79c77a16f6b
myname <******@gmail.com>
2016-09-02 17:50:49 -0700

    添加swagger依赖

--- src/main/java/com/serviceapi/ServiceAPIApp.java
+++ src/main/java/com/serviceapi/ServiceAPIApp.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 @RestController
 public class ServiceAPIApp {
 	
--- src/main/java/com/serviceapi/controller/UserController.java
+++ src/main/java/com/serviceapi/controller/UserController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.serviceapi.model.User;
 
-@Controller
+//@Controller
+@RestController
 @Scope("prototype")
 @RequestMapping(value = "/user") 
 public class UserController extends BaseController {
@@ -23,22 +25,22 @@ public class UserController extends BaseController {
 		return user;
 	}
 	
-	@RequestMapping(value = "/index")
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index() {
         return "Index Page";
     }
 	
-	@RequestMapping(value = "/{username}")
+	@RequestMapping(value = "/one/{username}", method = RequestMethod.GET)
 	public String userProfile(@PathVariable("username") String username) {
 	    return String.format("user %s", username);
 	}
 
-	@RequestMapping(value = "/update/{id}")
+	@RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
 	public String post(@PathVariable("id") int id) {
 	    return String.format("post %d", id);
 	}
 	
-	@RequestMapping("/hello/{name}")
+	@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
     public String hello(@PathVariable("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello";
